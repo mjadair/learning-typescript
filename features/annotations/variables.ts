b677b8665c486dd0f350bff5ba3e79c9e98ccd21
Michael Adair <mjadair91@gmail.com>
2020-07-27 12:53:39 +0100

    variable types

--- features/annotations/variables.ts
+++ features/annotations/variables.ts
@@ -1,6 +1,18 @@
-// annotating the specific type on the variable declaration
+//* type declaration is annotating the specific type on the variable declaration
 let apples: number = 5
 
+//* but we can generall avoid the above because TS has type inference
+let goodTimes = true
+
+//? goodTimes will have a type of Boolean because TS infers it from the declaration being on the same line as initialization
+
+//* this changes if we have initialization on a different line to declaration
+
+let mikesName
+mikesName = 'Michael'
+
+//? in the example above, Typescript thinks mikesName will have a type of 'any'
+ 
 // if we try to reassign this a value that's not a string, the compiler will complain.
 let speed: string = 'fast'
 
@@ -44,4 +56,15 @@ let fiat: Car = new Car()
 let point: { x: number, y: number} = {
   x: 10,
   y: 20
+}
+
+
+
+//! Function
+// we want to use a type annnotation to describe the function
+// We want to tell it what type of arguments the function is going to take
+// As well as what type of values we want it to return
+//? everything on the right hand side of the = is the actual function, the rest is type description of the function
+const logNumber: (i: number) => void = (i: number) => {
+  console.log(i)
 }
